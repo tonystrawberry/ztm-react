ed58f20ca42f0d051a808594a9d0a903560d7bd1
tonystrawberry <tony.duong.102@gmail.com>
2022-03-08 23:25:15 +0900

    feat: signin component

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={Shop} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
